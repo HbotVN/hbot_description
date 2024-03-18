647dd73487807aa3332c8abe5b1afe6bdfca782c
HuyNV-Pi4 <huyhust13@gmail.com>
2024-03-19 06:46:51 +0700

    update launch and gen urdf file

--- launch/hbot_description.launch.py
+++ launch/hbot_description.launch.py
@@ -5,7 +5,7 @@ import os
 
 def generate_launch_description():
   pkg_share = launch_ros.substitutions.FindPackageShare(package='hbot_description').find('hbot_description')
-  default_model_path = os.path.join(pkg_share, 'urdf', 'hbot.urdf')
+  default_model_path = os.path.join(pkg_share, 'urdf', 'hbot.urdf.xacro')
   default_rviz_config_path = os.path.join(pkg_share, 'rviz', 'hbot.rviz')
 
   print('default_model_path : {}'.format(default_model_path))
@@ -14,7 +14,8 @@ def generate_launch_description():
     package='robot_state_publisher',
     executable='robot_state_publisher',
     name='robot_state_publisher',
-    parameters=[{'robot_description': Command(['xacro', ' ', LaunchConfiguration('model')])}]
+    parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time'),
+        'robot_description': Command(['xacro', ' ', LaunchConfiguration('model')])}]
   )
   # joint_state_publisher_node = launch_ros.actions.Node(
   #   package='joint_state_publisher',
@@ -37,6 +38,8 @@ def generate_launch_description():
                                           description='Absolute path to rviz config file'),
     launch.actions.DeclareLaunchArgument(name='rviz', default_value='false',
                                           description='Open RViz?'),
+    launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='false',
+        description='Use simulation (Gazebo) clock if true'),
     # joint_state_publisher_node,
     robot_state_publisher_node,
     rviz_node
